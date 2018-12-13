50de6e64d1c8bb288eb591dc042bdd62d3fd1906
Noah Lange <noahrlange@gmail.com>
2018-12-13 15:28:14 -0600

    :wrench: Removes old paths from webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,35 @@
 const path = require('path');
 
+const common = {
+  resolve: {
+    extensions: ['.ts', '.js', '.json']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};
+
 module.exports = [
   {
+    ...common,
     entry: './src/entry/server.ts',
     output: {
       path: path.resolve(__dirname, 'scripts/server'),
       filename: 'server_addon.js'
-    },
-    resolve: {
-      alias: {
-        '@minecraft': path.resolve(__dirname, './src/minecraft/')
-      },
-      extensions: ['.ts', '.js', '.json']
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(ts|js)x?$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader'
-        }
-      ]
     }
   },
   {
+    ...common,
     entry: './src/entry/client.ts',
     output: {
       path: path.resolve(__dirname, 'scripts/client'),
       filename: 'addon.js'
-    },
-    resolve: {
-      alias: {
-        '@minecraft': path.resolve(__dirname, './src/minecraft/')
-      },
-      extensions: ['.ts', '.js', '.json']
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(ts|js)x?$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader'
-        }
-      ]
     }
   }
 ];
